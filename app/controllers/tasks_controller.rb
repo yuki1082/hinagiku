9804b6165719e3e66a4bc33b3c3ab9ac10cd1402
negishi <yuki071053@gmail.com>
2015-08-08 16:04:03 +0900

    second commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,36 @@ class TasksController < ApplicationController
 	def show
 		@task = Task.find(params[:id])
 	end 
+
+	def new
+		@task = Task.new
+	end 
+
+	def edit 
+		@task = Task.find(params[:id])
+	end 
+
+	def update
+		@task = Task.find(params[:id])
+		@task.update_attributes(params[:task])
+		redirect_to @task
+	end 
+
+	def create
+		@task = Task.create(params[:task])
+		redirect_to @task
+	end 
+
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+		redirect_to :tasks
+	end 
+
+	def finish
+		@task = Task.find(params[:id])
+		@task.update_attribute(:done,  true)
+		redirect_to :back
+	end 
+	
 end
